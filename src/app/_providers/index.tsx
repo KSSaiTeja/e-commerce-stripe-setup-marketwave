c27222254d8c7774d0101327c8fc992898648735
Kalepu Satya Sai Teja <mail@kssaiteja.me>
2024-04-07 19:36:41 +0530

    all issues fixed

--- src/app/_providers/index.tsx
+++ src/app/_providers/index.tsx
@@ -2,8 +2,9 @@
 
 import React from 'react'
 
-import { AuthProvider } from '../_providers/Auth'
-import { CartProvider } from '../_providers/Cart'
+import { AuthProvider } from './Auth'
+import { CartProvider } from './Cart'
+import { FilterProvider } from './Filter'
 import { ThemeProvider } from './Theme'
 
 export const Providers: React.FC<{
@@ -12,7 +13,9 @@ export const Providers: React.FC<{
   return (
     <ThemeProvider>
       <AuthProvider>
-        <CartProvider>{children}</CartProvider>
+        <FilterProvider>
+          <CartProvider>{children}</CartProvider>
+        </FilterProvider>
       </AuthProvider>
     </ThemeProvider>
   )
